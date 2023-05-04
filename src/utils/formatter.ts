409a3c8fcad32456c8fe9da1d5856f4fbba367cf
Matheus Messias <matheus.jf@live.com>
2023-05-04 17:21:10 -0300

    Final Adjustments

---
+++ src/utils/formatter.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+export const dateFormatter = new Intl.DateTimeFormat('pt-BR')
+
+export const priceFormatter = new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+})
